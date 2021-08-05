98dc24cbf758a8321555923f63cb5adde480578f
Ross McHardie <ross.mchardie@gmail.com>
2021-08-05 22:26:06 +0100

    Initial upload

---
+++ js/changeImage.js
@@ -0,0 +1,57 @@
+let num = 1;
+let previousBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+let image;
+let imageContainer = document.getElementById("workImg");
+let title = document.getElementById("imageModalTitle");
+
+function checkNum() {
+	if (num == 1) {
+		previousBtn.disabled = true;
+		previousBtn.className="btn btn-secondary";
+	} else {
+		previousBtn.disabled = false;
+		previousBtn.className="btn btn-outline-primary";
+	}
+
+	if (num == 6) {
+		nextBtn.disabled = true;
+		nextBtn.className="btn btn-secondary";
+	} else {
+		nextBtn.disabled = false;
+		nextBtn.className="btn btn-primary";
+	}
+}
+
+function showImage(clickedImage) {
+	image = "images/work" + clickedImage;
+	num = clickedImage;
+	imageContainer.src= image + ".jpg";
+	imageContainer.alt="Work Image" + num;
+	title.innerHTML="Work Image " + num;
+	checkNum();
+	console.log("Current image is: " + image);
+	console.log("Image ID: " + num);
+}
+
+function nextImage() {
+	num++;
+	checkNum();
+
+	imageContainer.src= "images/work" + num + ".jpg";
+	imageContainer.alt="Work Image" + num;
+	title.innerHTML="Work Image " + num;
+
+	console.log("Image ID: " + num);
+}
+
+function prevImage() {
+	num--;
+	checkNum();
+
+	imageContainer.src= "images/work" + num + ".jpg";
+	imageContainer.alt="Work Image" + num;
+	title.innerHTML="Work Image " + num;
+
+	console.log("Image ID: " + num);
+}

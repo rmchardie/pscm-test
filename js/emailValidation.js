3a3162576153fed01afee72407581047337be5b2
Rossco <67593221+rmchardie@users.noreply.github.com>
2021-10-22 17:46:40 +0100

    Add files via upload

--- js/emailValidation.js
+++ js/emailValidation.js
@@ -1,22 +1,35 @@
-const email = document.getElementById("email");
 const emailError = document.getElementById("invalidEmail");
 const emailSuccess = document.getElementById("validEmail");
 const spamMessage = document.getElementById("spamMessage");
+const spamFilter = document.getElementById("spamFilter");
+
+const nameField = document.querySelector('#name');
+const emailField = document.querySelector('#email');
+const textBox = document.querySelector('#name');
+const robotCheckbox = document.querySelector('#notRobot');
 
 function ValidateEmail(mail) 
 {
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
   {
-    console.log("Email valid");
     emailError.hidden = true;
     emailSuccess.hidden = false;
     spamMessage.hidden = true;
     return (true)
   } else {
-    console.log("Email invalid");
     emailSuccess.hidden = true;
     emailError.hidden = false;
     spamMessage.hidden = true;
     return (false)
   }
 }
+
+document.querySelector('#contactForm').addEventListener("submit", function(event) {
+  if (!robotCheckbox.checked || nameField.value === '' || emailField.value === '' || textBox === '') {
+    event.preventDefault();
+    spamFilter.hidden = false;
+  } else {
+    spamFilter.hidden = true;
+  }
+
+}, true);
